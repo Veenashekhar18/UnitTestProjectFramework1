efd70bc66d76c147cda193204557d8b01e3c3b6f
Veenashekhar18 <veenaraichur58@gmail.com>
2023-08-01 16:19:21 +0530

    datafetch

---
+++ UnitTestProjectFramework1/Attri13/DynamicdataandDatasource.cs
@@ -0,0 +1,41 @@
+﻿using Bytescout.Spreadsheet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProjectFramework1.Attributes
+{
+    [TestClass]
+    [TestCategory("Data")]
+    public class DynamicdataandDatasource
+    {
+        [TestMethod]
+        [TestCategory ("Data")]
+        [DynamicData(nameof(place),DynamicDataSourceType.Method)]
+        public void Dynamicdatafetch(string Place, string value)
+        {
+            Console.WriteLine($"place names are {Place} and values are {value}");
+        }
+
+        public static IEnumerable<object[]> place()
+        {
+            Spreadsheet spreadsheet = new Spreadsheet();
+            spreadsheet.LoadFromFile("D:\\VisualStudioRepos\\UnitTestProjectFramework1\\UnitTestProjectFramework1\\Repository\\C# data.xlsx");
+            var sheet = spreadsheet.Workbook.Worksheets["c# data"];
+            var maxrow = sheet.UsedRangeRowMax;
+            var maxcol = sheet.UsedRangeColumnMax;
+            for (int i = 0; i < maxrow; i++)
+            {
+                string Place = sheet.Cell(i, 0).ToString();
+                string value = sheet.Cell(i, 1).ToString();
+                yield return new object[] { Place, value };
+            }
+
+
+
+        }
+    }
+}
