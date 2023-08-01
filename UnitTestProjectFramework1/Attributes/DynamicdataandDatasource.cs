ae611203673e6dd608431703368044de21859b00
Veenashekhar18 <veenaraichur58@gmail.com>
2023-08-01 16:18:45 +0530

    datadriven

---
+++ UnitTestProjectFramework1/Attributes/DynamicdataandDatasource.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProjectFramework1.Attributes
+{
+    [TestClass]
+    [TestCategory("data")]
+    public class DynamicdataandDatasource
+    {
+        [TestMethod]
+        [DynamicData(nameof(places),DynamicDataSourceType.Method)]
+        public void Dynamicdata(string places,string value)
+        {
+            Console.WriteLine($"Attributes are {places} and positions are {values}");
+        }
+
+        public static IEnumerable<object[]> Places()
+        {
+            Spreadsheet spreadsheet=new Spreadsheet();
+
+
+    }
+}
