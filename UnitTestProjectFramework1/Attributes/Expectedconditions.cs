aa566568fd7574b562ee039643afe97497189392
Veenashekhar18 <veenaraichur58@gmail.com>
2023-08-01 13:17:06 +0530

    Expected conditions and Timeout

---
+++ UnitTestProjectFramework1/Attributes/Expectedconditions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProjectFramework1.Attributes
+{
+    [TestClass]
+    public class Expectedconditions
+    {
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void ExpecctedExcen()
+        {
+            string[] places =new string[] { "Mysore", "Banglore", "Mumbai", "Delhi", "Manali" };
+            for(int i = 0; i <= places.Length; i++)
+            {
+                Console.WriteLine(places[i]);
+            }
+        }
+
+    }
+}
---
+++ UnitTestProjectFramework1/Attributes/Timeoutandowner.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnitTestProjectFramework1.Attributes
+{
+    [TestClass]
+    [TestCategory("Attributes")]
+    public class Timeoutandowner
+    {
+        [TestMethod,TestCategory("TimeOut"),Owner("veena")]
+        [Timeout(1000)]
+        public void Timeoutattr() 
+        {
+           Thread.Sleep(1000);
+        }
+    }
+}
